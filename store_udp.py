86a6ef0cc41dc964fe07086509c6a51594c7ca52
Danchisama <dante.u.o@gmail.com>
2019-04-14 07:39:31 +0000

    ACK ok, falta insert MySQL

--- store_udp.py
+++ store_udp.py
@@ -14,10 +14,10 @@ import socket
 # Constant LMU ACK
 Service_Type = '02'
 Message_Type = '01'
-Type_ = '01'
+Type_ = '02'
 ACK_ = '00'
 Spare_ = '00'
-AppVersion_ = '000000'
+AppVersion_ = '0000'
 
 
 clienteIP = '23.239.5.206'
@@ -49,6 +49,8 @@ UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 UDPServerSocket.bind(("23.239.5.206",5000))
 print("UDP server up and listening")
 
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
 # Listen for incoming datagrams
 while(True):
     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
@@ -56,11 +58,14 @@ while(True):
     #print "bytesAddressPair", bytesAddressPair
 
     message1 = bytesAddressPair[0]
+    #print "Message1: ", message1
+
     addr_port = bytesAddressPair[1]
     address = addr_port[0]
     port_addr = addr_port[1]
 
     message = binascii.hexlify(message1)
+    #print "Message: ", message
 
     clientMsg = "Message from Client:{}".format(message)
     clientIP  = "Client IP Address:{}".format(address)
@@ -89,28 +94,29 @@ while(True):
 
     Sequence = message[22:26]
     #print "Sequence#:", Sequence
+    #Sequence = '0001'
 
     Update_Time = (datetime.datetime.fromtimestamp(int(int(message[26:34], 16))).strftime('%d-%m-%Y %H:%M:%S'))
-    print "Update Time:", Update_Time
+    #print "Update Time:", Update_Time
 
     TimeOfFix = (datetime.datetime.fromtimestamp(int(int(message[34:42], 16))).strftime('%d-%m-%Y %H:%M:%S'))
-    print "TimeOfFix:", TimeOfFix
+    #print "TimeOfFix:", TimeOfFix
 
     Lat = message[42:50]
     if  int(Lat[0:1],16) > 7:
         Latitude = round((-0.0000001*(bit_not(int(Lat,16))+1)),7)
-        print "Latitude:", Latitude
+        #print "Latitude:", Latitude
     else:
         Latitude = round((0.0000001*(bit_not(int(Lat,16)))),7)
-        print "Latitude:", Latitude
+        #print "Latitude:", Latitude
 
     Long = message[50:58]
     if  int(Long[0:1],16) > 7:
         Longitude = round((-0.0000001*(bit_not(int(Long,16))+1)),7)
-        print "Longitude:", Longitude
+        #print "Longitude:", Longitude
     else:
         Longitude = round((0.0000001*(bit_not(int(Long,16)))),7)
-        print "Longitude:", Longitude
+        #print "Longitude:", Longitude
 
     #Altitude = message[58:66]
     #print "Altitude:", Altitude
@@ -154,8 +160,10 @@ while(True):
     #User_Msg_Length = message[102:106]
     #print "User Msg Length:", User_Msg_Length
 
+    #print "Message: ", message[106:((2*(int(message[102:106],16)))+106-2)]
+
     User_Msg = binascii.unhexlify(message[106:((2*(int(message[102:106],16)))+106-2)])
-    print "User Msg:", User_Msg
+    #print "User Msg:", User_Msg
 
     AckMessage = OptionsByte + MobileIDLength + MobileID + MobileIDLen + MobileIDType + Service_Type + Message_Type + Sequence + Type_ + ACK_ + Spare_ + AppVersion_
 
@@ -167,34 +175,34 @@ while(True):
     myList = []
     mac = items[0]
     #mac = mac[0:len(mac)-2]
-    print "MAC: ", mac
+    #print "MAC: ", mac
 
     humidity = items[1]
-    print "Humidity: ", humidity
+    #print "Humidity: ", humidity
 
     humidity2 = items[2]
-    print "Humidity2: ", humidity2
+    #print "Humidity2: ", humidity2
 
     temperature = items[3]
-    print "Temperature: ", temperature
+    #print "Temperature: ", temperature
 
     temperature2 = items[4]
-    print "Temperature2: ", temperature2
+    #print "Temperature2: ", temperature2
 
     ammonia = items[5]
-    print "Ammonia: ", ammonia
+    #print "Ammonia: ", ammonia
 
     ammonia2 = items[6]
-    print "Ammonia2: ", ammonia2
+    #print "Ammonia2: ", ammonia2
 
     speed = items[7]
-    print "Speed: ", speed
+    #print "Speed: ", speed
 
     co2 = items[8]
-    print "CO2: ", co2
+    #print "CO2: ", co2
 
     battery = items[9]
-    print "Battery: ", battery
+    #print "Battery: ", battery
 
     # creating sql insert sentence
     insertSQL = "insert into data"
@@ -202,18 +210,25 @@ while(True):
     insertValuePH = "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s"
     sqlValues = [TimeOfFix, Latitude, Longitude, mac, humidity, humidity2, temperature, temperature2, ammonia, ammonia2, speed, co2, battery]
     insertSQL = insertSQL + "(" + insertFields + ") values (" + insertValuePH + ")"
-    
-    connection = mysql.connect()
-    cursor = connection.cursor()
-    cursor.execute(insertSQL,tuple(sqlValues))
-    connection.commit()
+
+    #connection = mysql.connect()
+    #cursor = connection.cursor()
+    #cursor.execute(insertSQL,tuple(sqlValues))
+    #connection.commit()
 
     msgFromClient = AckMessage
-    bytesToSend = str.encode(msgFromClient)
-    serverAddressPort = (clienteIP, 5000)
+    print "AckMessage: ", AckMessage
+
+    bytesToSend = binascii.unhexlify(msgFromClient)
+    #print "bytesToSend: ", bytesToSend
+
+    serverAddressPort = (address, port_addr)
 
     # Send to server using created UDP socket
     UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
+
+
 
 
 def setupLogger():
